758b7717820578e320923ef58bc2d8e2e6639ebe
Rafael Jacov Medel <medelrafjac@gmail.com>
2022-10-06 21:55:57 +0800

    ATM.py line 10: reverted for loop to while loop

--- ATM.py
+++ ATM.py
@@ -7,22 +7,24 @@ from __module import main
 
 print("ATM SIMULATION - Banko De UNO")
 
-for v in range(3):
-    if v == 3:
+verify_counter = 0
+
+while True:
+    if verify_counter == 3:
         print("Your card has been confiscated due to entering incorrect pin three times...")
         exit()
 
     try:
         pin = int(input('Enter PIN: '))
     except:
-        v += 1
+        verify_counter += 1
         print("Incorrect Pin!\n")
     else:
         if len(str(pin)) == 6:
             choose_tran()
             break
         else:
-            v += 1
+            verify_counter += 1
             print("Incorrect Pin!\n")
 
 
